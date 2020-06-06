aed97cc4d983c62e13bc5652cbc41e6304c2661d
wuwanneng <wanneng.wu@baozun.com>
2020-06-06 09:26:20 +0800

    测试多线程测试类

---
+++ src/main/java/com/baozun/test/ThreadTest.java
@@ -0,0 +1,13 @@
+package com.baozun.test;
+
+/**
+ * @ProjectName: springboot
+ * @Package: com.baozun.test
+ * @ClassName: ThreadTest
+ * @Author: wwn
+ * @Description: 测试多线程
+ * @Date: 2020/6/6 9:25
+ * @Version: 1.0
+ */
+public class ThreadTest {
+}
